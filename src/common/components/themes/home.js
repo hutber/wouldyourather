04931042a6f64a015614cdc416ca116736155149
Jamie Hutber <jamie@hutber.com>
2018-12-07 17:04:55 +0000

    feat: 🎸 add home page with theme

---
+++ src/common/components/themes/home.js
@@ -0,0 +1,15 @@
+export default theme => ({
+	content: {
+		background: '#000'
+	},
+	root: {
+		flexGrow: 1
+	},
+	paper: {
+		height: 140,
+		width: 100
+	},
+	control: {
+		padding: theme.spacing.unit * 2
+	}
+})
